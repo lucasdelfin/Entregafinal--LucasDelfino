8ab8ad3bf8b5ede46ebfe0c9379eaf52d3317070
lucas delfino <lucasdel.7@hotmail.com>
2022-09-21 23:28:38 -0300

    primer Commit

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,9 @@
+import "../styles/ItemListContainer.scss";
+
+function ItemListContainer({greeting}) {
+  return (
+    <div className="titulo">{greeting}</div>
+  )
+}
+
+export default ItemListContainer
---
+++ src/components/NavBar.js
@@ -0,0 +1,40 @@
+
+import "../styles/NavBar.scss"
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import CardWidget from "./CardWidget";
+
+
+function NavBar() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+            <Container>
+                <Navbar.Brand href="#home">Inicio</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Productos</Nav.Link>
+                        <Nav.Link href="#pricing">Contacto</Nav.Link>
+                        <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                Another action
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Nosotros</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                Separated link
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <CardWidget/>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+
+}
+
+export default NavBar
